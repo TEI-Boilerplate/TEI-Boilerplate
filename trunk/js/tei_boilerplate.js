688f84b134b351dea9b2ab7eee11263059504442
GrantLS <GrantLS@3bbcc0c9-51c9-4ab4-afdd-1b50544f6a2e>
2012-04-13 01:04:51 +0000

    changes from grant-exp branch
    git-svn-id: https://subversion.assembla.com/svn/tei_boilerplate@77 3bbcc0c9-51c9-4ab4-afdd-1b50544f6a2e

--- trunk/js/tei_boilerplate.js
+++ trunk/js/tei_boilerplate.js
@@ -1,1 +1,43 @@
-$("note").addClass("closed");
+/*
+$(document).ready(function() {
+	alert("hey");
+	$("html > head > title").text($("TEI > teiHeader > fileDesc > titleStmt > title:first").text());
+	$('pb').each(function(index) {
+		$(this).text("[page: " + $(this).attr('n') + "]");
+	});
+	$.unblockUI();				
+});
+*/
+
+function getPageBreaks(){
+	return document.getElementsByTagName('pb');
+}
+
+function clearPageBreaks(){
+	var pageBreaks = getPageBreaks();
+	for(pageBreak in pageBreaks){
+		pageBreaks[pageBreak].textContent = '';
+	}
+}
+
+function addPageBreaks(){
+	var pageBreaks = getPageBreaks();
+	for(pageBreak in pageBreaks){
+		if(null != pageBreaks[pageBreak].attributes.getNamedItem('n')){
+			pageBreaks[pageBreak].textContent = "[page: " 
+				+ pageBreaks[pageBreak].attributes.getNamedItem('n').value
+				+ "]";
+		}
+	}
+}
+
+document.getElementById('pbToggle').onclick = function(){
+	if(document.getElementById('pbToggle').checked){
+		clearPageBreaks();
+	}else{
+		addPageBreaks();
+	}
+};
+
+addPageBreaks();
+document.getElementById('pbToggle').checked = false;

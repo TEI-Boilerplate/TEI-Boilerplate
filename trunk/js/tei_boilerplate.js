d174f20994117eff65bb4fab1a6107b728a58a5c
GrantLS <GrantLS@3bbcc0c9-51c9-4ab4-afdd-1b50544f6a2e>
2012-04-13 02:20:12 +0000

    tweaked code to add event handler. pb should now work in Safari, FF, and Chrome
    git-svn-id: https://subversion.assembla.com/svn/tei_boilerplate@82 3bbcc0c9-51c9-4ab4-afdd-1b50544f6a2e

--- trunk/js/tei_boilerplate.js
+++ trunk/js/tei_boilerplate.js
@@ -1,14 +1,3 @@
-/*
-$(document).ready(function() {
-	alert("hey");
-	$("html > head > title").text($("TEI > teiHeader > fileDesc > titleStmt > title:first").text());
-	$('pb').each(function(index) {
-		$(this).text("[page: " + $(this).attr('n') + "]");
-	});
-	$.unblockUI();				
-});
-*/
-
 function getPageBreaks(){
 	return document.getElementsByTagName('pb');
 }
@@ -23,7 +12,8 @@ function clearPageBreaks(){
 function addPageBreaks(){
 	var pageBreaks = getPageBreaks();
 	for(pageBreak in pageBreaks){
-		if(null != pageBreaks[pageBreak].attributes.getNamedItem('n')){
+		if(null != pageBreaks[pageBreak].attributes.getNamedItem('n')
+			&& undefined != pageBreaks[pageBreak].attributes.getNamedItem('n')){
 			pageBreaks[pageBreak].textContent = "[page: " 
 				+ pageBreaks[pageBreak].attributes.getNamedItem('n').value
 				+ "]";
@@ -31,13 +21,25 @@ function addPageBreaks(){
 	}
 }
 
-document.getElementById('pbToggle').onclick = function(){
-	if(document.getElementById('pbToggle').checked){
-		clearPageBreaks();
-	}else{
-		addPageBreaks();
-	}
-};
+function init(){
+	document.getElementById('pbToggle').onclick = function(){
+		if(document.getElementById('pbToggle').checked){
+			clearPageBreaks();
+		}else{
+			addPageBreaks();
+		}
+	};
+	addPageBreaks();
+	document.getElementById('pbToggle').checked = false;
+}
 
-addPageBreaks();
-document.getElementById('pbToggle').checked = false;
+//If W3C event model used, prefer that. Window events are fallbacks
+if(document.addEventListener){
+	//W3C event model used
+	document.addEventListener("DOMContentLoaded", init, false);
+	window.addEventListener("load", init, false);
+} else if(document.attachEvent){
+	//IE event model used
+	document.attachEvent( "onreadystatechange", init);
+	window.attachEvent( "onload", init);
+}
